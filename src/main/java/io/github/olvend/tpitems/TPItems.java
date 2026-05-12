fd12c60c4a2d72efe146b11ae7f2f3d1be8659d8
olvend <olve@nystad.net>
2026-05-12 14:11:33 +0200

    Initial commit

---
+++ src/main/java/io/github/olvend/tpitems/TPItems.java
@@ -0,0 +1,15 @@
+package io.github.olvend.tpitems;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TPItems implements ModInitializer {
+    public static final String MOD_ID = "tpitems";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Hello Fabric world!");
+    }
+}
